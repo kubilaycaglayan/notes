01ef2670861a809671a85171ea06509466b8c65b
Kubilay Caglayan <kblycaglayan@gmail.com>
2020-04-15 18:57:54 +0300

    File added ror

--- 2-ruby/2-practising/regex.rb
+++ 2-ruby/2-practising/regex.rb
@@ -1,3 +1,13 @@
+text = 
+<<~TITLE
+Sabahlara akdar dayanamam osman aga
+ben bugün çok mutluyum
+12.01.2012 545
+something.rb
+  is there anybody out there?
+TITLE
+
+
 p "Do you like cats and dogs?" =~ /like/
 puts "Do you like cats and dogs?".match(/like/)
 def contains_vowel(str)
@@ -6,3 +16,9 @@ end
 contains_vowel("vowely")
 contains_vowel("shhhh")
 
+def contains(str, regexp)
+    puts index = str =~ regexp
+    puts str[index..-1]
+end
+contains("vowely", /o/)
+contains(text, /\n\s/)
