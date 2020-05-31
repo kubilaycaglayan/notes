f1faa2ef59d75e90a863d0baf7dfd7d7473764cb
Kubilay <kblycaglayan@gmail.com>
2020-05-31 12:51:35 +0300

    Commit for all

--- 3-ror/basic_session/app/controllers/users_controller.rb
+++
@@ -1,6 +0,0 @@
-class UsersController < ApplicationController
-  def index
-    @users = User.all
-    #session[:current_user_id] = 2
-  end
-end
--- 3-ror/followers/app/models/user.rb
+++
@@ -1,4 +0,0 @@
-class User < ApplicationRecord
-	has_and_belongs_to_many :follower, class_name: "User", foreign_key: "follower_id"
-	has_and_belongs_to_many :leader, class_name: "User", foreign_key: "leader_id"
-end
