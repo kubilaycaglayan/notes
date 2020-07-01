682e50159fa67e146b8dbf1ccd44858a28529cb2
Kubilay <kblycaglayan@gmail.com>
2020-07-01 18:02:50 +0300

    Add webpack-tutorial

--- 5-javascript/webpack-demo/src/index.js
+++ 5-javascript/webpack-demo/src/index.js
@@ -1,11 +1,13 @@
 import _ from 'lodash';
+import myName from './myName';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
   // Lodash is now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpack '], ' ');
+  element.innerHTML += myName('Kubilay');
 
   return element;
 }
---
+++ 5-javascript/webpack-tutorial/src/index.js
@@ -0,0 +1,3 @@
+import name from './foo';
+
+console.log('Helslso World!s', name, 'Hhahah');
