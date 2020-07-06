a9d60705c9fa66be5fe8b68fbd0a7bb81197de9c
kubilaycaglayan <kblycaglayan@gmail.com>
2020-07-06 10:29:44 +0300

    Sandpipers solutions

--- 4-algorithms/sanpipers/scratch.rb
+++ 4-algorithms/sanpipers/scratch.rb
@@ -13,19 +13,19 @@ end
 
 class Node
   attr_accessor :value, :next_node
-  
+
   def initialize(value, next_node = nil)
 	  @value = value
     @next_node = next_node
   end
 end
 
-node1 = Node.new(1991)
-node2 = Node.new(2001)
-
-node1.next_node = node2
-
-print_objects_w_message(Node, 'Created first node...')
+# node1 = Node.new(1991)
+# node2 = Node.new(2001)
+#
+# node1.next_node = node2
+#
+# print_objects_w_message(Node, 'Created first node...')
 
 class LinkedList
   def add(value)
@@ -33,8 +33,38 @@ class LinkedList
     #EDGE CASES
     # 1-) my linked list is empty
     #     - make @head equals to new_node
-  end 
+    #     - make @tail equals to new_node
+    # 2-) if my LL is not empty
+    #     - change tail
+    #     - change the last node's NN equals to the new node
+
+    if @head.nil?
+      @head = new_node
+      @tail = new_node
+    else
+      @tail.next_node = new_node
+      @tail = new_node
+    end
+  end
 
   def get(index)
+    node = @head
+
+    while index > 0 && node
+      node = node.next_node
+      index -= 1
+    end
+
+    node
   end
 end
+
+list = LinkedList.new
+list.add('Marios')
+list.add('Iryn')
+list.add('Frank')
+p list
+
+# p list.get(2)
+# p list.get(1)
+# p list.get(0)
