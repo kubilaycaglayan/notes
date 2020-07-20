00c17578ce0739fb7e7b878c2302e3428f66c266
kubilaycaglayan <kblycaglayan@gmail.com>
2020-07-20 22:15:07 +0300

    Add and fetch api example

---
+++ 5-javascript/fetch/post/uploadFile.js
@@ -0,0 +1,17 @@
+const formData = new FormData();
+const fileField = document.querySelector('input[type="file"]');
+
+formData.append('username', 'abc123');
+formData.append('avatar', fileField.files[0]);
+
+fetch('https://example.com/profile/avatar', {
+  method: 'PUT',
+  body: formData
+})
+.then(response => response.json())
+.then(result => {
+  console.log('Success:', result);
+})
+.catch(error => {
+  console.error('Error:', error);
+});
