b288a170d9e263884cae901a15c31291730971ba
kubilaycaglayan <kblycaglayan@gmail.com>
2020-09-29 14:08:48 +0300

    Hey ho

--- 5-javascript/promises/example_a.js
+++ 5-javascript/promises/example_a.js
@@ -1,29 +1,10 @@
-const numberDelay = new Promise( function(resolve, reject){
-  setTimeout(
-    () => { resolve(10) },
-    1000
-  )
-})
-
 const number = new Promise( function(resolve, reject){
   resolve(10)
 })
 
-console.log(number);
-
-function printNumberDelay() {
-  numberDelay.then(
-    (result) => { console.log(result, 'With setTimeOut'); },
-  );
-}
-
-function printNumber() {
-  number.then(
-    (result) => { console.log(result); },
-  );
-}
-
-printNumber();
-printNumberDelay()
-
-console.log('Amazing, always async');
+number
+.then(
+  res => {
+    console.log(res)
+  }
+)
